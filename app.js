83883c82d3d55880c9c9304bc313072d53dfc4bd
Matt J <matt.jackels@gmail.com>
2017-11-30 17:55:49 -0600

    change port from 8000 to port

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.use('/', indexRoutes);
 app.use('/campgrounds/:id/comments', commentRoutes);
 app.use('/campgrounds', campgroundRoutes);
 
-app.listen(8000, function(){
+app.listen(port, function(){
     console.log("serving yelpcamp v7 on port 8000");
 });
